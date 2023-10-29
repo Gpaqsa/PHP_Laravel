14a8ca7ea9171de0b30c3c90d7c841498bebb41e
Gpaqsa <giorgi.paksashvili.2@btu.edu.ge>
2023-10-29 22:53:11 +0400

    Fixed some bugs

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -7,5 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Quiz extends Model
 {
-    use HasFactory;
+    $quiz = new Quiz();
+    $quiz->name = 'My Quiz';
+    $quiz->questions = 'Question 1, Question 2';
+    $quiz->options = 'Option A, Option B, Option C';
+    $quiz->save();
+
+
+    $quiz = Quiz::find(1); 
+    $quiz->name = 'Updated Quiz Name';
+    $quiz->save();
+
+
+    $quizzes = Quiz::all();
+
+    $quiz = Quiz::find(1); 
+    if ($quiz) {
+        $quiz->delete();
+    } else {
+    }
+
 }
