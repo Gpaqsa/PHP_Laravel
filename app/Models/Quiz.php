2987b57720493c3ce9cec9605bc8de8647c27aa5
Gpaqsa <giorgi.paksashvili.2@btu.edu.ge>
2023-10-29 22:57:39 +0400

    Add route

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -5,26 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Quiz extends Model
-{
-    $quiz = new Quiz();
-    $quiz->name = 'My Quiz';
-    $quiz->questions = 'Question 1, Question 2';
-    $quiz->options = 'Option A, Option B, Option C';
-    $quiz->save();
+class Quiz extends Model{}
+$quiz = new Quiz();
+$quiz->name = 'My Quiz';
+$quiz->questions = 'Question 1, Question 2';
+$quiz->options = 'Option A, Option B, Option C';
+$quiz->save();
 
 
-    $quiz = Quiz::find(1); 
-    $quiz->name = 'Updated Quiz Name';
-    $quiz->save();
+$quiz = Quiz::find(1); 
+$quiz->name = 'Updated Quiz Name';
+$quiz->save();
 
 
-    $quizzes = Quiz::all();
+$quizzes = Quiz::all();
 
-    $quiz = Quiz::find(1); 
-    if ($quiz) {
-        $quiz->delete();
-    } else {
-    }
-
+$quiz = Quiz::find(1); 
+if ($quiz) {
+    $quiz->delete();
+} else {
 }
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-// Route::get('quizForm/{id?}', 'QuizController'); //@ShowForm
-// Route::post('/quizForm', 'QuizController'); //@SubmitForm
+Route::get('quizForm/{id?}', 'QuizController'); //@ShowForm
+Route::post('/quizForm', 'QuizController'); //@SubmitForm
